ac3ec488512e9216bea8823549c07a04340e1076
maguaya <47848802+maguaya@users.noreply.github.com>
2019-04-17 23:26:27 +0300

    added a list

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+	c=a+b
+	return c
+
+def subtract(a,b):
+	c=a-b
+	return c
+
+def multiply(a,b):
+	 c=a*b
+	 return c
+
+def divide(a,b):
+	c=a/b
+	return c
+
+def modulu(a,b):
+	c=a%b
+	return c
+
+def percentage(a,b):
+	c=a/b*100
+	return c
+
+def whole_number_division(a,b):
+	c=a//b
+	return c
+
+def exponent(a,b):
+    c=a**b
+    return c		
