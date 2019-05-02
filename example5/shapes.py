5c21b48f074c9a71e84d6caaedbaf0c7c3dd6682
maguaya <47848802+maguaya@users.noreply.github.com>
2019-05-02 19:17:05 +0300

    example 4 added

---
+++ example5/shapes.py
@@ -0,0 +1,70 @@
+class circle(object):
+	def_init_(self,radius,area,circumference):
+
+	         self.radius=radius
+	         self.arae=area
+	         self.circumference
+	 def area(self,A):
+            A=3.14*
+	         return A
+	   def circumference(self,C):
+            C=2(3.14)*2
+            
+            return C
+
+            class square(object):
+            	
+            	def __init__(self,a,p):
+            		
+            		self.a=a
+            		self.p=p
+            		def area(a*2):
+
+            		w+l+h
+            		return a
+
+            		def p(self):
+            		p=L+W*2
+            		return p
+
+
+            		class sphere(object):
+            			
+            			def __init__(self,area,volume);
+            				
+                              self.area=arae
+                              self.volume=volume
+
+                              def area(self,A):
+                                   A=4(3.14*2)
+                                       return A
+
+                        def volume(self,V)
+                                V=4(3.14*3)
+                                return V
+
+
+
+
+
+
+                          
+
+            	
+            				
+
+            	
+            	
+            	
+            		
+            		
+
+
+
+	
+		
+		
+		
+ 
+		
+		
