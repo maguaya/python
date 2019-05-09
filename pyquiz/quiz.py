f555698cf197347d207c9f737c3b035c666e62df
maguaya <47848802+maguaya@users.noreply.github.com>
2019-05-09 18:58:54 +0300

    added file

---
+++ pyquiz/quiz.py
@@ -0,0 +1,44 @@
+def  (x):
+	x=[100,110,120,130,140,150]
+         x%3
+
+def sorted(a,b,c):
+    a=['apple','banana','mango',]
+    b=['avacado','peaach','orange']
+    c=['pineapple','lemon']
+        d=a+b+c
+         print(c)
+
+ def divisible_by_3(n): 
+        for n in b:
+	if n % 3 ==1:
+		print("{} is doubled by 3" .format (n))
+           
+
+def (x):
+	listx=[[1,2],[3,4],[5,6] ]
+	    flat=[]
+    for x in nested:
+	for x in y:
+		v=y
+		print(v)
+
+def smallest():
+	min([1,3,6,8,2,4,5.7])
+
+def remove (x):
+	x=['a','b','a','e','d','b','c','f','g','h']
+       x.remove(a,b,e)
+	
+   def squares():
+   	  x= range(149,159)
+   	  print( rang())
+       
+for x in range(len(list)):
+    
+   for b in x:
+		print(b)
+	   
+
+					
+		
